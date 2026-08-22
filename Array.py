b16e1255868675bb51a76599e41642816e97f328
karthik <karthikeyagudimalla2007@gmail.com>
2026-08-22 10:47:01 +0530

    added more array funtions

--- Array.py
+++ Array.py
@@ -2,13 +2,24 @@
 
 import array as arr
 
+#creating an array
 a=arr.array('i',[1,2,3,4,5])
 
+#printing the array we declared
 print("The Original Array is: ",a)
 
+#inserting an element at the end of the array
 a.append(50)
 print("After appending array is: ",a)
 
-removed_element=a.pop()
+#removing an element at specific positon
+removed_element=a.pop(1)
 print(f"Removing last element: {removed_element}")
 print("Array after removing last element: ",a)
+
+#inserting an element at a specific position
+a.insert(2,15)
+print("adding 5 at positon 2: ",a)
+
+#length of the array
+print("The length of the array is ",len(a))
